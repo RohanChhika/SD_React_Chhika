efafb22eacc720ffaae9eb6b652f47b67a34c718
babaYonisali <2460755@students.wits.ac.za>
2024-04-18 23:14:53 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState, useCallback} from "react";
 import logo from './logo.svg';
 import './App.css';
 import LoginButton from "./components/LoginButton";
