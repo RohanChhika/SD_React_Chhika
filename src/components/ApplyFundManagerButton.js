4d005da8ced4ecc54d4f3778a8421cdfdc5779a2
RohanChhika <2543404@students.wits.ac.za>
2024-04-25 21:41:52 +0200

    Fund Final

--- src/components/ApplyFundManagerButton.js
+++ src/components/ApplyFundManagerButton.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+
 
 
 const ApplyFundManagerButton = () => {
