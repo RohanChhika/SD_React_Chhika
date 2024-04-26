9f634f2e8bb2c7e8679ba7b06c88a27fcccaa173
RohanChhika <2543404@students.wits.ac.za>
2024-04-26 17:01:02 +0200

    Foley

--- src/components/FundManagerApplication.js
+++ src/components/FundManagerApplication.js
@@ -1,53 +1,53 @@
 import React, { useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const FundManagerApplication = () => {
   const [motivation, setMotivation] = useState('');
-  const {isAuthenticated, user, getAccessTokenSilently } = useAuth0();
-  const history = useHistory();
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+  const navigate = useNavigate(); // Using useNavigate instead of useHistory
+
   const handleChange = (e) => {
     setMotivation(e.target.value);
   };
 
-  const handleSubmit =async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!isAuthenticated) {
       alert('You must be logged in to submit your application.');
       return;
     }
   
-    const userID = user.sub; // Assuming you have access to user object
+    const userID = user.sub;
     const data = {
       userID,
       motivation
     };
   
     try {
-      const accessToken = await getAccessTokenSilently(); // Fetch the access token silently
+      const accessToken = await getAccessTokenSilently();
       const response = await fetch('https://fundit.azurewebsites.net/managerRequest', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}` // Include the Authorization header with the Bearer token
+          Authorization: `Bearer ${accessToken}`
         },
         body: JSON.stringify(data)
       });
   
       const responseData = await response.json();
       if (response.status === 409) {
-        alert('A request with this user ID already exists.'); // Display an alert if status is 409
+        alert('A request with this user ID already exists.');
       } else if (response.ok) {
         console.log('Request created successfully:', responseData);
-        alert("Request made successfully")
-       history.push('/');
-        // Additional success handling logic here
+        alert("Request made successfully");
+        navigate('/'); // Redirect to the index route
       } else {
         throw new Error(responseData.message || 'Failed to create request');
       }
     } catch (error) {
       console.error('Failed to submit motivation:', error.message);
-      alert('Error: ' + error.message); // Display an alert for other errors
+      alert('Error: ' + error.message);
     }
   };
 
