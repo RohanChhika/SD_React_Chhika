fee87ac20aa3ce2cb0f69522ad053d8e96a56222
NaftaliDiner <2144598@students.wits.ac.za>
2024-04-30 23:02:12 +0200

    AdminFundReq CSS 1

--- src/components/AdminFundReq.js
+++ src/components/AdminFundReq.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+import logo from './components/Images/logo1.png';
 import { useNavigate } from "react-router-dom";
 const AdminFundReq = () => {
   const [selectedMotivation, setSelectedMotivation] = useState(null);
@@ -97,31 +98,43 @@ const AdminFundReq = () => {
   };
 
   return (
-    <>
-      <h1 className='admin-page' style={{ textAlign: 'center' }}>Admin Page</h1>
-      <h2 className='admin-page' style={{ textAlign: 'center' }}>Motivations List</h2>
-      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-        <select className="motivation-select" style={{ width: '300px', textAlign: 'center' }} onChange={(e) => handleSelectMotivation(JSON.parse(e.target.value))}>
-          <option value="">Select a motivation</option>
-          {motivations.map(motivation => (
-            <option key={motivation.userID} value={JSON.stringify(motivation)}>
-              {motivation.motivation}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      {selectedMotivation && (
-        <div className='motivation-detail' style={{ width: '600px', border: '1px solid #ccc', padding: '10px', textAlign: 'left', marginBottom: '20px', margin: '0 auto' }}>
-          <h3>User ID: {selectedMotivation.userID}</h3>
-          <p>{selectedMotivation.motivation}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="center-text">
+          <h1>Fund Requests</h1>
         </div>
-      )}
-      <div className="button-container" style={{ textAlign: 'center' }}>
-        <button className='button' style={{ marginRight: '10px' }} onClick={handleAcceptMotivation} disabled={!selectedMotivation}>Accept</button>
-        <button className='button' onClick={handleDeclineMotivation} disabled={!selectedMotivation}>Decline</button>
-      </div>
-    </>
+      </header>
+      <main>
+        <h1 className='admin-page' style={{ textAlign: 'center' }}>Admin Page</h1>
+        <h2 className='admin-page' style={{ textAlign: 'center' }}>Motivations List</h2>
+        <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+          <select className="motivation-select" style={{ width: '300px', textAlign: 'center' }} onChange={(e) => handleSelectMotivation(JSON.parse(e.target.value))}>
+            <option value="">Select a motivation</option>
+            {motivations.map(motivation => (
+              <option key={motivation.userID} value={JSON.stringify(motivation)}>
+                {motivation.motivation}
+              </option>
+            ))}
+        </select>
+        </div>
+
+        {selectedMotivation && (
+          <div className='motivation-detail' style={{ width: '600px', border: '1px solid #ccc', padding: '10px', textAlign: 'left', marginBottom: '20px', margin: '0 auto' }}>
+            <h3>User ID: {selectedMotivation.userID}</h3>
+            <p>{selectedMotivation.motivation}</p>
+          </div>
+        )}
+        <div className="button-container" style={{ textAlign: 'center' }}>
+          <button className='button' style={{ marginRight: '10px' }} onClick={handleAcceptMotivation} disabled={!selectedMotivation}>Accept</button>
+          <button className='button' onClick={handleDeclineMotivation} disabled={!selectedMotivation}>Decline</button>
+        </div>
+      </main>
+
+      <footer className="App-footer">
+      © 2024 FundIT. All rights reserved.
+    </footer>
+    </div>
   );
 };
 
--- src/components/FundManagerApplication.js
+++ src/components/FundManagerApplication.js
@@ -52,7 +52,7 @@ const FundManagerApplication = () => {
   };
 
   return (
-    <div>
+    <div className="main-content">
       <h1 style={{textAlign: 'center'}}>Apply to be a Fund Manager</h1>
       <h2 style={{textAlign: 'center'}}>Please provide your motivation</h2>
       <form className='fund-manager-form' onSubmit={handleSubmit}>
