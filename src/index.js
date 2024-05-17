46ec0791790bb21b43847a557b7adc0767fadfe0
Ismaaeel11 <2435817@students.wits.ac.za>
2024-05-17 17:10:15 +0200

    index comms

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter } from 'react-router-dom';
 
+// Create a root element to render the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* Wrap the application in BrowserRouter for routing */}
     <BrowserRouter>
+    {/* Wrap the application in Auth0Provider for authentication */}
       <Auth0Provider
         domain="dev-32ptb8idfvaxx33n.eu.auth0.com"
         clientId="ZHgmcL9KH5oGlIAKUpR7NRthesGEKOrV"
@@ -17,6 +20,7 @@ root.render(
           audience: "http://fundit.azurewebsites.net/"
         }}
       >
+        {/* Render the main App component */}
         <App />
       </Auth0Provider>
     </BrowserRouter>
