c6c2eddfbb26dd16a4fb1a3d3e8758f1ae8f9d32
Ismaaeel11 <2435817@students.wits.ac.za>
2024-05-19 18:36:04 +0200

    ViewmyFundButtonn

--- src/components/buttons/ViewMyFundButton.js
+++ src/components/buttons/ViewMyFundButton.js
@@ -3,35 +3,45 @@ import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const ViewMyFundButton = () => {
+  // Destructure necessary functions and variables from useAuth0 hook
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  // State to store user information
   const [userInfo, setUserInfo] = useState(null);
+  // Ref to store user role
   const userRoleRef = useRef('');
 
   useEffect(() => {
+    // Function to fetch user data from the server
     const fetchUserData = async () => {
       try {
+        // Check if the user is authenticated and user.sub exists
         if (!isAuthenticated || !user?.sub) return;
 
+        // Get the access token silently
         const token = await getAccessTokenSilently();
+        // Fetch user data from the server
         const response = await fetch('https://fundit.azurewebsites.net/login', {
-          method: 'POST',
+          method: 'POST',   // Specify POST method
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${token}`    // Include the access token in the headers
           },
-          body: JSON.stringify({
-            userID: user.sub
+          body: JSON.stringify({  // Set content type to JSON
+            userID: user.sub    // Include user ID in the request body
           })
         });
 
+        // Check if the response is not ok
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
+        // Parse and set the user data
         const data = await response.json();
         console.log('User data:', data); // Log user data to inspect it
         setUserInfo(data);
-        userRoleRef.current = data.role; // Update userRoleRef instead of userRole
+        // Update userRoleRef with the role from the user data
+        userRoleRef.current = data.role;
       } catch (error) {
         console.error('Failed to fetch user data:', error);
       }
@@ -44,9 +54,10 @@ const ViewMyFundButton = () => {
 
   return (
     <>
+    {/* Render the View My Funds Status button only if userInfo is available and user role is 'applicant' */}
       {userInfo && userRoleRef.current === 'applicant' && (
-        <Link to="/fund-status">
-          <button className='button'>View My Funds Status</button>
+        <Link to="/fund-status">    {/* Link to the fund-status route */}
+          <button className='button'>View My Funds Status</button>  {/* Button to view fund status */}
         </Link>
       )}
     </>
