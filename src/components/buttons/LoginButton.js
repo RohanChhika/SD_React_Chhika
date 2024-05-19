7673358c0fd26042986dc662e229ad9c535f1a81
Ismaaeel11 <2435817@students.wits.ac.za>
2024-05-19 18:53:33 +0200

    Login/Logout

--- src/components/buttons/LoginButton.js
+++ src/components/buttons/LoginButton.js
@@ -2,26 +2,31 @@ import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = ({onUserAdded}) => {
+  // Destructure necessary functions and variables from useAuth0 hook
   const { loginWithRedirect, isAuthenticated, user, getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
+    // Function to add the user to the database
     const addUserToDatabase = async () => {
       if (!user) return; // Only proceed if the user is defined
       try {
+        // Get the access token silently
         const token = await getAccessTokenSilently();
+        // Send a POST request to add the user to the database
         const response = await fetch('https://fundit.azurewebsites.net/signup', {
-          method: 'POST',
+          method: 'POST',   // Specify POST method
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
           },
           body: JSON.stringify({
             userID: user.sub, // Auth0 user identifier
-            role: 'applicant',
+            role: 'applicant',  // Default role assigned to the user
             contact: user.email // User email from Auth0
           })
         });
-
+        
+        // Check if the response is not ok
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -30,14 +35,17 @@ const LoginButton = ({onUserAdded}) => {
       } catch (error) {
         console.error('Failed to add user to the database:', error);
       }
+      // Call the onUserAdded callback function
       onUserAdded();
     };
 
     if (isAuthenticated) {
+      // Call addUserToDatabase function if the user is authenticated
       addUserToDatabase();
     }
   }, [isAuthenticated, user, getAccessTokenSilently, onUserAdded]);
 
+  // Render the Log In button only if the user is not authenticated
   return !isAuthenticated && (
     <button className="button" onClick={() => loginWithRedirect()}>
       Log In
--- src/components/buttons/LogoutButton.js
+++ src/components/buttons/LogoutButton.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
+  // Destructure logout function and isAuthenticated boolean from useAuth0 hook
     const { logout , isAuthenticated} = useAuth0();
   
     return (
+      // Render the Logout button only if the user is authenticated
         isAuthenticated &&(
       <button className="button" onClick={() => logout({ returnTo: window.location.origin })}>
         Log Out
