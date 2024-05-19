2943d87dc1a22fca630462c83080ad6f644d6178
Ismaaeel11 <2435817@students.wits.ac.za>
2024-05-19 18:30:36 +0200

    ViewFundRequestButtton

--- src/components/buttons/ViewMyFundRequestsButtton.js
+++ src/components/buttons/ViewMyFundRequestsButtton.js
@@ -3,16 +3,23 @@ import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const ViewMyFundRequestsButton = () => {
+  // Destructure necessary functions and variables from useAuth0 hook
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  // State to store user information
   const [userInfo, setUserInfo] = useState(null);
+  // Ref to store user role
   const userRoleRef = useRef('');
 
   useEffect(() => {
+    // Function to fetch user data from the server
     const fetchUserData = async () => {
       try {
+        // Check if the user is authenticated and user.sub exists
         if (!isAuthenticated || !user?.sub) return;
 
+        // Get the access token silently
         const token = await getAccessTokenSilently();
+        // Fetch user data from the server
         const response = await fetch('https://fundit.azurewebsites.net/login', {
           method: 'POST',
           headers: {
@@ -24,10 +31,12 @@ const ViewMyFundRequestsButton = () => {
           })
         });
 
+        // Check if the response is not ok
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-
+        
+        // Parse and set the user data
         const data = await response.json();
         console.log('User data:', data); // Log user data to inspect it
         setUserInfo(data);
@@ -44,9 +53,10 @@ const ViewMyFundRequestsButton = () => {
 
   return (
     <>
+      {/* Render the View My Fund Requests button only if userInfo is available and user role is 'manager' */}
       {userInfo && userRoleRef.current === 'manager' && (
-        <Link to="/view-my-fund-requests">
-          <button className='button'>View My Fund Requests</button>
+        <Link to="/view-my-fund-requests">  {/* Link to the view-my-fund-requests route */}
+          <button className='button'>View My Fund Requests</button> {/* Button to view fund requests */}
         </Link>
       )}
     </>
