626bb43a3c31efad0bfbd8e0bb14ca1892523328
Ismaaeel11 <2435817@students.wits.ac.za>
2024-05-21 12:39:41 +0200

    about us information

--- src/components/AboutUsTile.js
+++ src/components/AboutUsTile.js
@@ -3,8 +3,19 @@ import aboutUsImage from './Images/about-us.png';
 
 const AboutUsTile = () => {
     return (
-        <section className="about-us-section" style={{ padding: '0', margin: '120px' }}>
-            <img src={aboutUsImage} alt="About Us" />
+        <section className="about-us-section">
+            <div className="image-container">
+                <img src={aboutUsImage} alt="About Us" />
+            </div>
+            <article className="about-us-article">
+                <h1>About Us</h1>
+                <p>
+                    Welcome to our company! We are dedicated to providing the best services in the industry. Our team of experts is committed to excellence and delivering funding catering to your needs.
+                </p>
+                <p>
+                    We provide funding in the form of bursaries, healthcare, and business. To learn more or apply for funding, please sign up and explore the variety of funding opportunities that we offer.
+                </p>
+            </article>
         </section>
     );
 };
