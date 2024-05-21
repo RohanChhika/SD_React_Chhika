3276cb409a50e2247998314ed28809bf5420727a
Ismaaeel11 <2435817@students.wits.ac.za>
2024-05-21 12:19:07 +0200

    about

--- src/components/AboutUsTile.js
+++ src/components/AboutUsTile.js
@@ -1,14 +1,12 @@
 import React from "react";
-import aboutUsImage from './Images/about-us.png'
+import aboutUsImage from './Images/about-us.png';
 
-const AboutUsTile = () =>
-    {
-        return (
-            <section className = "about-us-section">
-                <img src = {aboutUsImage} alt = "About Us" />
-
-            </section>
-        );
-    };
+const AboutUsTile = () => {
+    return (
+        <section className="about-us-section" style={{ padding: '0', margin: '0' }}>
+            <img src={aboutUsImage} alt="About Us" />
+        </section>
+    );
+};
 
 export default AboutUsTile;
